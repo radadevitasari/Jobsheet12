a2b58504f5d1ed24f80527dbf8d57b4fa90c77e8
radadevitasari <radadevitasari87.com>
2025-11-26 14:05:47 +0700

    modifikasi no 2

--- Kafe19.java
+++ Kafe19.java
@@ -1,6 +1,7 @@
 public class Kafe19 {
 
-    public static void Menu() {
+    public static void main(String[] args) {
+        
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam Rp 15,000");
         System.out.println("2. Cappuccino Rp 20,000");
@@ -11,9 +12,6 @@ public class Kafe19 {
         System.out.println("============================");
         System.out.println("Silakan pilih menu yang Anda inginkan.");
     }
-
-    public static void main(String[] args) {
-        Menu(); 
-    }
 }
+
     
