a028afa7dae5e6deda042c16d6487935da2f8061
radadevitasari <radadevitasari87.com>
2025-11-26 16:28:12 +0700

    modifikasi no 2

--- PengunjungCafe19.java
+++ PengunjungCafe19.java
@@ -2,12 +2,13 @@ public class PengunjungCafe19 {
 
     static void daftarPengunjung(String... namaPengunjung) {
         System.out.println("Daftar Nama Pengunjung:");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- " + namaPengunjung[i]);
+
+        for (String nama : namaPengunjung) {
+            System.out.println("- " + nama);
         }
+        System.out.println(); 
     }
     public static void main(String[] args) {
-
         daftarPengunjung("Ali", "Budi", "Citra");
         
         daftarPengunjung("Andi");
