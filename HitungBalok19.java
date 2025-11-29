d3672724053eb9fc8592aeac4cc7954f3f9d3114
radadevitasari <radadevitasari87.com>
2025-11-29 19:58:45 +0700

    soal no 3

--- HitungBalok19.java
+++ HitungBalok19.java
@@ -2,6 +2,16 @@ import java.util.Scanner;
 
 public class HitungBalok19 {
 
+    static int hitungLuas(int pjg, int lb) {
+        int Luas = pjg * lb;
+        return Luas;
+    }
+
+    static int hitungVolume(int tinggi, int a, int b) {
+        int Volume = hitungLuas(a, b) * tinggi;
+        return Volume;
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int p, l, t, L, vol;
@@ -15,13 +25,12 @@ public class HitungBalok19 {
         System.out.println("Masukkan tinggi: ");
         t = input.nextInt();
 
-        L = p * l;
+        L = hitungLuas(p, l);
         System.out.println("Luas persegi panjang adalah " + L);
 
-        vol = p * l * t;
-        System.out.println("Volume balok adalah " + vol);
+        vol = hitungVolume(t, p, l);
+        System.out.println("Volume Balok adalah " + vol);
 
         input.close();
     }
 }
-
